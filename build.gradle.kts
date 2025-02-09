9dd2accf02cc105ac3dc684c84aa008c6fb84676
David <david@cappell.net>
2025-02-09 23:56:13 +0100

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.shadow)
+}
+
+allprojects {
+
+    group = "io.github.solid-resourcepack"
+    version = "1.0"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "com.github.johnrengelman.shadow")
+
+    dependencies {
+        testImplementation(rootProject.libs.kotlin.test)
+        implementation(rootProject.libs.kotlin.jvm)
+    }
+
+    kotlin {
+        jvmToolchain(21)
+        compilerOptions {
+            apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        }
+    }
+
+    tasks.named("shadowJar", ShadowJar::class) {
+        mergeServiceFiles()
+        archiveFileName.set("${project.name}.jar")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+}
